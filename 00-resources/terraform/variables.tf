76f72bb9a471d47ae1aa5325d25f87ce58b0f341
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-04-14 16:04:11 -0500

    Update variables.tf

--- 00-resources/terraform/variables.tf
+++ 00-resources/terraform/variables.tf
@@ -2,17 +2,27 @@ variable "project_id" {
   type        = string
   description = "project id required"
 }
+
 variable "project_number" {
  type        = string
  description = "project number in which demo deploy"
 }
+
 variable "user_principal_name" {
  description = "Your GCP user ID"
 }
+
 variable "org_id" {
  description = "Organization ID in which project created"
 }
-variable "gcp_region" {
- description = "GCP region"
+
+variable "bq_region" {
+ description = "BQ multi-region"
+ default = "US"
+}
+
+variable "dataproc_region" {
+ description = "GCP region for Dataproc"
+ default = "us-central1"
 }
 
